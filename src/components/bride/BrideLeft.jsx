bc71116b32b436ab7927254c1357e9f4af1878a4
kopsus <emonsetio01@gmail.com>
2024-08-15 23:26:26 +0700

    add butterfly and dummy API

--- src/components/bride/BrideLeft.jsx
+++ src/components/bride/BrideLeft.jsx
@@ -1,43 +1,61 @@
 import Image from "next/image";
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
-import leftBride from "@/assets/image/personal/leftBride.png";
 import bottom_right from "@/assets/image/flores/bottom_right.jpg";
 import { southernaire, nightStillComes, alice } from "@/utils/fonts";
+import imageMempelaiPria from "@/assets/image/personal/leftBride.png";
+import Link from "next/link";
+import kupuSmall from "@/assets/image/butterfly/kupu_small.gif";
 
 const BrideLeft = () => {
-  return (
-    <div className="relative top-0 w-full h-full">
+  const biodataMempelaiPria = [
+    {
+      namaPanggilan: "Iyan",
+      namaLengakp: "Bagus Pratama. M, Sos",
+      namaOrtu:
+        "Putra Kedua dari Bapak H. Maratua dan Ibu Hj. Yunar Lisma Pohan",
+      linkIg: "https://instagram.com",
+      potoProfile: imageMempelaiPria,
+    },
+  ];
+
+  return biodataMempelaiPria.map((value, index) => (
+    <div key={index} className="relative w-full h-full">
       <Image src={bottom_right} alt="" width={0} height={0} />
       <div className="absolute top-0 w-full h-full">
         <div className="px-7 pt-5 flex flex-col items-end">
           <p className={`${southernaire.className} text-primary text-6xl`}>
-            Iyan
+            {value.namaPanggilan}
           </p>
           <div className="mt-3 mb-5 flex flex-col items-end">
             <p
               className={`text-3xl text-(rgba(41,41,41)) ${nightStillComes.className}`}
             >
-              Bagus Pratama. M, Sos
+              {value.namaLengakp}
             </p>
             <p
               className={`${alice.className} text-gray-500 text-lg text-end w-[65%]`}
             >
-              Putra Kedua dari Bapak H. Maratua dan Ibu Hj. Yunar Lisma Pohan
+              {value.namaOrtu}
             </p>
           </div>
-          <FaInstagram
-            color="white"
-            size={36}
-            className="bg-primary p-1 rounded-full"
-          />
+          <Link href={value.linkIg} target="_blank">
+            <FaInstagram
+              color="white"
+              size={36}
+              className="bg-primary p-1 rounded-full cursor-pointer"
+            />
+          </Link>
+        </div>
+        <div className="w-full">
+          <Image src={value.potoProfile} alt="" width={0} height={0} />
+        </div>
+        <div className="overflow-hidden w-48 absolute bottom-72 right-0">
+          <Image src={kupuSmall} alt="" width={0} height={0} />
         </div>
       </div>
-      <div className="absolute w-full bottom-0">
-        <Image src={leftBride} alt="" width={0} height={0} />
-      </div>
     </div>
-  );
+  ));
 };
 
 export default BrideLeft;
--- src/components/bride/BrideRight.jsx
+++ src/components/bride/BrideRight.jsx
@@ -3,38 +3,56 @@ import floreTopLeft from "@/assets/image/flores/top_left.jpg";
 import { FaInstagram } from "react-icons/fa";
 import rightBride from "@/assets/image/personal/rightBride.png";
 import { southernaire, nightStillComes, alice } from "@/utils/fonts";
+import Link from "next/link";
+import kupuSmall from "@/assets/image/butterfly/kupu_small.gif";
 
 const BrideRight = () => {
-  return (
-    <div className="relative top-0 w-full h-full">
+  const biodataMempelaiWanita = [
+    {
+      namaPanggilan: "Nia",
+      namaLengakp: "Riza Cantika. M, Sos",
+      namaOrtu:
+        "Putra Kedua dari Bapak H. Maratua dan Ibu Hj. Yunar Lisma Pohan",
+      linkIg: "https://instagram.com",
+      potoProfile: rightBride,
+    },
+  ];
+
+  return biodataMempelaiWanita.map((value, index) => (
+    <div key={index} className="relative top-0 w-full h-full">
       <Image src={floreTopLeft} alt="" width={0} height={0} />
       <div className="absolute top-0 w-full h-full">
-        <div className="px-7 pt-14">
+        <div className="px-7 pt-5 flex flex-col items-start">
           <p className={`${southernaire.className} text-primary text-6xl`}>
-            Nia
+            {value.namaPanggilan}
           </p>
-          <div className="mt-3 mb-5">
+          <div className="mt-3 mb-5 flex flex-col">
             <p
-              className={`${nightStillComes.className} text-3xl text-(rgba(41,41,41))`}
+              className={`text-3xl text-(rgba(41,41,41)) ${nightStillComes.className}`}
             >
-              Riza Cantika. M, Sos
+              {value.namaLengakp}
             </p>
             <p className={`${alice.className} text-gray-500 text-lg w-[65%]`}>
-              Putra Kedua dari Bapak H. Maratua dan Ibu Hj. Yunar Lisma Pohan
+              {value.namaOrtu}
             </p>
           </div>
-          <FaInstagram
-            color="white"
-            size={36}
-            className="bg-primary p-2 rounded-full"
-          />
+          <Link href={value.linkIg} target="_blank">
+            <FaInstagram
+              color="white"
+              size={36}
+              className="bg-primary p-1 rounded-full cursor-pointer"
+            />
+          </Link>
+        </div>
+        <div className="w-full">
+          <Image src={value.potoProfile} alt="" width={0} height={0} />
+        </div>
+        <div className="overflow-hidden w-48 absolute bottom-64 left-20">
+          <Image src={kupuSmall} alt="" width={0} height={0} />
         </div>
       </div>
-      <div className="absolute w-full bottom-0">
-        <Image src={rightBride} alt="" width={0} height={0} />
-      </div>
     </div>
-  );
+  ));
 };
 
 export default BrideRight;
