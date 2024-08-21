a88d616e57278ccbf58d14ecf3597069adebc842
kopsus <emonsetio01@gmail.com>
2024-08-21 20:11:16 +0700

    create type for data

---
+++ src/api/types/index.ts
@@ -0,0 +1,74 @@
+export type typeMempelai = {
+  id: string;
+  nama_lengkap_pria: string;
+  nama_lengkap_Wanita: string;
+  nama_panggilan_pria: string;
+  nama_panggilan_wanita: string;
+  nama_orang_tua_pria: string;
+  nama_orang_tua_wanita: string;
+  instagram_pria: string;
+  instagram_wanita: string;
+  timeline: string;
+  foto_pria: string;
+  foto_wanita: string;
+};
+
+export type typeCover = {
+  id: string;
+  image: string[];
+};
+
+export type typeBanner2 = {
+  id: string;
+  image: string;
+};
+
+export type typeProgram = {
+  id: string;
+  judul: string;
+  tanggal: string;
+  waktu_mulai: string;
+  waktu_berakhir: string;
+  tempat: string;
+  maps: string;
+};
+
+export type typeLove = {
+  id: string;
+  image: string;
+};
+
+export type typeStory = {
+  id: string;
+  judul: string;
+  tanggal: string;
+  story: string;
+};
+
+export type typeWaktuSubmit = {
+  id: string;
+  nama: string;
+  ucapan: string;
+  konfirmasi_kehadiran: string;
+  waktu_submit: string;
+};
+
+export type typeYtLink = {
+  id: string;
+  link: string;
+};
+
+export type typeGallery = {
+  id: string;
+  image: string;
+};
+
+export type typeFooter = {
+  id: string;
+  image: string;
+};
+
+export type typeAudio = {
+  id: string;
+  image: string;
+};
